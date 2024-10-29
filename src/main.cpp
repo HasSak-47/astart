a878da7e004cc388ac2b8d4db00bb6757df6ea20
daniel-nix <ulises.alanis.255@gmail.com>
2024-10-28 18:55:18 -0600

    panic!

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <cstdlib>
 #include <iostream>
 #include <vector>
@@ -25,10 +26,50 @@ struct Map{
         nodes[r].childs.push_back(Node::Child{d, &nodes[b]});
         nodes[b].childs.push_back(Node::Child{d, &nodes[r]});
     }
+
+    bool is_end(size_t id) {
+        return id == 10;
+    }
+
+    double heuristic(size_t id) {
+        return 0.;
+    } 
+
+    size_t start(){
+        return 0;
+    }
 };
+
+struct FFI{ char bytes[40]; };
+struct FFI_AStart{ char bytes[78]; };
+
+
+FFI_AStart new_world(FFI world) ;
+
+void start(FFI_AStart* start);
+bool next(FFI_AStart * start, size_t* val) ;
+
+FFI new_ffi(
+    void* obj,
+    void* start,
+    void* get_neightbors,
+    void* heuristic,
+    void* is_end
+);
+
+
 
 int main() {
     Map map = {};
     map.populate_and_link();
+
+    FFI ffi = new_ffi(
+        (void*)&map,
+        NULL,
+        NULL,
+        NULL,
+        NULL
+    );
+
     return 0;
 }
